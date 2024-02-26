2a828ee98b67ce1a99d2aa7940db26796d3b1426
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-02-26 00:26:11 -0800

    Added files
    This is all Tasks of Oasis Infobyte

---
+++ Task_3_Temperature/script.js
@@ -0,0 +1,31 @@
+const celsiusElem = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusElem.innerHTML = "";
+});
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+});
+
+function convertToCelsius() {
+  let inputValue = parseFloat(degree.value);
+  if (isNaN(inputValue)) {
+    alert("Please enter a valid number.");
+    return;
+  }
+  if (tempType.value === "fahrenheit") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} °C`;
+  } else if (tempType.value === "kelvin") {
+    const KelvinToCelsius = inputValue - 273.15;
+    celsiusElem.innerHTML = `${KelvinToCelsius.toFixed(3)} °C`;
+  } else {
+    alert("Please select a valid temperature type.");
+  }
+}
